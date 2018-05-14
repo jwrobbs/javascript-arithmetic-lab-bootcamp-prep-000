e4f705a423e13cc2ad6594b43cae2c6395b64ae8
Josh Robbs <josh@rustbeltwebworks.com>
2018-05-14 18:18:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ function add(a,b) {
 function subtract(a, b) {
   return a - b
 }
+function multiply(a, b) {
+  return a * b
+}
+function divide(a, b) {
+  return a / b
+}
+function inc(a) {
+  a++
+  return a
+}
